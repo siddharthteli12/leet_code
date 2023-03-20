2571f645ec90dd51159dd7c3e9b2433a39c7b1a0
siddharthteli12 <telisiddharth123@yahoo.in>
2023-03-20 12:28:21 +0530

    Add: valid_parentheses lib.

---
+++ valid_parentheses/src/lib.rs
@@ -0,0 +1,42 @@
+pub fn is_valid(s: String) -> bool {
+    if s.len() <= 1 || s.len() % 2 != 0 {
+        println!("Inside first if, s.len --, {:?}", s.len());
+        return false;
+    }
+    let s_vec: Vec<char> = s.chars().collect();
+    let mut stack: Vec<char> = Vec::new();
+    for item in s_vec.iter() {
+        match item {
+            '(' | '{' | '[' => stack.push(*item),
+            ')' | '}' | ']' => match stack.pop() {
+                Some(n) => {
+                    if match_closing_parenthese(n, *item) {
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+                None => return false,
+            },
+            _ => {
+                unreachable!();
+            }
+        }
+    }
+
+    if stack.is_empty() {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+pub fn match_closing_parenthese(parenthese1: char, parenthese2: char) -> bool {
+    print!("Par1 {:}, Par2 {:} .", parenthese1, parenthese2);
+    match (parenthese1, parenthese2) {
+        ('(', ')') => true,
+        ('{', '}') => true,
+        ('[', ']') => true,
+        _ => false,
+    }
+}
