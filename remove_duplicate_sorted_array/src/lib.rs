e96368bd45cc208c1d80e7bf536942145996b737
siddharthteli12 <telisiddharth123@yahoo.in>
2023-05-14 23:02:45 +0530

    Refactor: (remove duplicate sorted array) Update nums vec rather than temp vec.

--- remove_duplicate_sorted_array/src/lib.rs
+++ remove_duplicate_sorted_array/src/lib.rs
@@ -1,23 +1,17 @@
 pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
     let mut counter = 1;
-    let mut non_duplicate_list: Vec<i32> = vec![];
     let mut pointer = match nums.get(0) {
-        Some(val) => {
-            non_duplicate_list.push(val.clone());
-            val.clone()
-        }
+        Some(val) => val.clone(),
         None => return 0,
     };
     for num in nums.clone() {
         if num == pointer {
             continue;
         } else {
-            non_duplicate_list.push(num);
+            nums[counter as usize] = num;
             pointer = num;
             counter = counter + 1;
         }
     }
-    nums.clear();
-    nums.append(&mut non_duplicate_list);
     counter
 }
