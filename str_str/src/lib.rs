78445f020368f49b6e62f59f71f3e88f1f454002
siddharthteli12 <telisiddharth123@yahoo.in>
2023-05-23 23:12:05 +0530

    Add: (str_str) Test cases.

--- str_str/src/lib.rs
+++ str_str/src/lib.rs
@@ -4,3 +4,36 @@ pub fn str_str(haystack: String, needle: String) -> i32 {
         Some(n) => n as i32,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_with_empty_strings() {
+        let haystack = String::new();
+        let needle = String::new();
+        assert_eq!(str_str(haystack, needle), 0);
+    }
+
+    #[test]
+    fn test_without_substring() {
+        let haystack = String::from("Siddharth");
+        let needle = String::from("Teli");
+        assert_eq!(str_str(haystack, needle), -1);
+    }
+
+    #[test]
+    fn test_with_simple_strings() {
+        let haystack = String::from("Siddharth");
+        let needle = String::from("Sid");
+        assert_eq!(str_str(haystack, needle), 0);
+    }
+
+    #[test]
+    fn test_with_simple_strings_2() {
+        let haystack = String::from("Siddharth");
+        let needle = String::from("dha");
+        assert_eq!(str_str(haystack, needle), 3);
+    }
+}
