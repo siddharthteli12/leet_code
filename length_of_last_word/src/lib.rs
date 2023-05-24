4312cf13ee6c395451ef3806e6f09b752b026f7c
siddharthteli12 <telisiddharth123@yahoo.in>
2023-05-24 23:32:50 +0530

    Refactor: (length of last words) Use rsplit.

--- length_of_last_word/src/lib.rs
+++ length_of_last_word/src/lib.rs
@@ -1,14 +1,32 @@
 pub fn length_of_last_word(s: String) -> i32 {
-    let string_vec = s.split_whitespace().collect::<Vec<&str>>();
-    match string_vec.last() {
-        Some(str) => str.len() as i32,
-        None => 0 as i32,
+    let s = s.trim();
+    match s.rsplit_once(" ") {
+        Some((_, last)) => last.len() as i32,
+        None => s.len() as i32,
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_with_empty_string() {
+        let sentence = String::new();
+        assert_eq!(length_of_last_word(sentence), 0);
+    }
+
+    #[test]
+    fn test_with_single_string() {
+        let sentence = String::from("Hello");
+        assert_eq!(length_of_last_word(sentence), 5);
+    }
+
+    #[test]
+    fn test_with_single_string_with_whitespace() {
+        let sentence = String::from("    Hello   ");
+        assert_eq!(length_of_last_word(sentence), 5);
+    }
 
     #[test]
     fn test_with_strings() {
