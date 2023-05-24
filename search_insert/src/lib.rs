3ed610f7a91d23b2c22402764a62cf43aa13acbc
siddharthteli12 <telisiddharth123@yahoo.in>
2023-05-24 22:56:05 +0530

    Refactor: (search_insert) binary search with vec utility.

--- search_insert/src/lib.rs
+++ search_insert/src/lib.rs
@@ -1,18 +1,7 @@
 pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    let mut left_index: i32 = 0;
-    let mut right_index: i32 = (nums.len() - 1) as i32;
-    let mut middle_index: i32;
-    while left_index <= right_index {
-        middle_index = (left_index + right_index) / 2;
-        if nums[middle_index as usize] == target {
-            return middle_index as i32;
-        } else if nums[middle_index as usize] > target {
-            right_index = middle_index - 1;
-        } else {
-            left_index = middle_index + 1;
-        }
-    }
-    left_index as i32
+    nums.binary_search(&target)
+        .map(|index| index as i32)
+        .unwrap_or_else(|err_index| err_index as i32)
 }
 
 #[cfg(test)]
