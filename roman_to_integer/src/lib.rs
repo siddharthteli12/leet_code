52734ffa3ab9da34c363648ee7e25c340e50c06b
Siddharth Teli <telisiddharth123@yahoo.in>
2023-08-22 18:15:28 +0530

    Refactor: Optimize.

--- roman_to_integer/src/lib.rs
+++ roman_to_integer/src/lib.rs
@@ -1,51 +1,46 @@
 pub fn roman_to_int(s: String) -> i32 {
-    let string_vec: Vec<char> = s.chars().collect();
-    let mut total: i32 = 0;
-    let string_length = string_vec.len();
-    for i in 0..string_length {
-        total += match string_vec[i] {
-            'I' => {
-                if i < string_length - 1 {
-                    match string_vec[i + 1] {
-                        'V' | 'X' => -1,
-                        _ => 1,
-                    }
-                } else {
-                    1
-                }
-            }
-            'X' => {
-                if i < string_length - 1 {
-                    match string_vec[i + 1] {
-                        'L' | 'C' => -10,
-                        _ => 10,
-                    }
-                } else {
-                    10
-                }
-            }
-            'C' => {
-                if i < string_length - 1 {
-                    match string_vec[i + 1] {
-                        'D' | 'M' => -100,
+    let mut result;
+    let mut iter = s.chars();
 
-                        _ => 100,
-                    }
-                } else {
-                    100
+    if let Some(mut previous_roman) = iter.next() {
+        result = convert_roman_to_num(previous_roman);
+        for roman_char in iter {
+            match (previous_roman, roman_char) {
+                ('I', 'V' | 'X') => {
+                    calculate_result_utility(previous_roman, roman_char, &mut result);
                 }
+                ('X', 'L' | 'C') => {
+                    calculate_result_utility(previous_roman, roman_char, &mut result);
+                }
+                ('C', 'D' | 'M') => {
+                    calculate_result_utility(previous_roman, roman_char, &mut result);
+                }
+                (_, _) => result += convert_roman_to_num(roman_char),
             }
-            'V' => 5,
-            'L' => 50,
-            'D' => 500,
-            'M' => 1000,
-            _ => {
-                unreachable!();
-            }
-        };
+            previous_roman = roman_char;
+        }
+        result
+    } else {
+        0
     }
+}
 
-    total
+pub fn convert_roman_to_num(roman_char: char) -> i32 {
+    match roman_char {
+        'I' => 1,
+        'V' => 5,
+        'X' => 10,
+        'L' => 50,
+        'C' => 100,
+        'D' => 500,
+        'M' => 1000,
+        _ => unreachable!(),
+    }
+}
+
+fn calculate_result_utility(previous_roman: char, roman_char: char, result: &mut i32) {
+    let value = convert_roman_to_num(roman_char) - 2 * convert_roman_to_num(previous_roman);
+    *result += value;
 }
 
 #[cfg(test)]
