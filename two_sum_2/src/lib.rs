5cc0d7e6589d2bb35d5d9ff15bb94e54726af4f1
Siddharth Teli <telisiddharth123@yahoo.in>
2023-09-06 18:32:13 +0530

    Refactor: Optimize, remove redundant line.

--- two_sum_2/src/lib.rs
+++ two_sum_2/src/lib.rs
@@ -1,11 +1,14 @@
 pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
     for (index, num) in numbers.iter().enumerate() {
         let compliment = target - num;
+        let mut start = 0;
+        let mut end = numbers.len();
         if compliment >= *num {
-            if let Ok(index2) = numbers[(index + 1)..].binary_search(&compliment) {
-                return vec![(index + 1) as i32, (index + index2 + 2) as i32];
-            }
-        } else if let Ok(index2) = numbers[..index].binary_search(&compliment) {
+            start = index + 1;
+        } else {
+            end = index;
+        }
+        if let Ok(index2) = numbers[start..end].binary_search(&compliment) {
             return vec![(index + 1) as i32, (index + index2 + 2) as i32];
         }
     }
@@ -20,5 +23,11 @@ mod tests {
     fn test_with_simple_numbers() {
         let numbers = vec![2, 7, 11, 15];
         assert_eq!(two_sum(numbers, 9), vec![1, 2]);
+    }
+
+    #[test]
+    fn test_with_simple_numbers_2() {
+        let numbers = vec![0, 0, 3, 4];
+        assert_eq!(two_sum(numbers, 0), vec![1, 2]);
     }
 }
