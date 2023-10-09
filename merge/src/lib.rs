e4844f88b36897660d378d688d64f24625ae4349
siddharthteli12 <telisiddharth123@yahoo.in>
2023-10-09 19:42:03 +0530

    Refactor: Without sort.

--- merge/src/lib.rs
+++ merge/src/lib.rs
@@ -1,11 +1,24 @@
 pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-    let mut i = nums1.len() - 1;
-    for num in nums2 {
-        nums1[i] = *num;
-        i -= 1;
+    let nums1_copy = nums1[..m as usize].to_vec();
+    nums1.clear();
+    let mut i = 0;
+    let mut j = 0;
+
+    while i < m as usize && j < n as usize {
+        if nums1_copy[i] < nums2[j] {
+            nums1.push(nums1_copy[i]);
+            i += 1;
+        } else {
+            nums1.push(nums2[j]);
+            j += 1;
+        }
     }
 
-    nums1.sort();
+    if i < m as usize {
+        nums1.extend_from_slice(&nums1_copy[i..m as usize]);
+    } else {
+        nums1.extend_from_slice(&nums2[j..n as usize]);
+    }
 }
 #[cfg(test)]
 mod tests {
