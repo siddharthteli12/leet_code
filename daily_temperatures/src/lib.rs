3fcbae232abb9c88015b0596e3ae4a30d1ca271a
siddharthteli12 <telisiddharth123@yahoo.in>
2023-11-18 10:15:33 +0530

    Refactor: Unoptimze solution

--- daily_temperatures/src/lib.rs
+++ daily_temperatures/src/lib.rs
@@ -2,14 +2,12 @@ pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
     let mut stack: Vec<(usize, i32)> = Vec::with_capacity(temperatures.len());
     let mut result = vec![0; temperatures.len()];
     for (index, temperature) in temperatures.iter().enumerate() {
-        let mut counter = 0;
-        while counter < stack.len() {
-            if stack[counter].1 < *temperature {
-                let value: (usize, i32) = stack.remove(counter);
-                result[value.0] = (index - value.0) as i32;
-            } else {
-                counter += 1;
-            }
+        let (targets, remaining): (Vec<(usize, i32)>, Vec<(usize, i32)>) =
+            stack.into_iter().partition(|&val| val.1 < *temperature);
+        stack = remaining;
+
+        for (tar_index, _) in targets {
+            result[tar_index] = (index - tar_index) as i32;
         }
         stack.push((index, *temperature));
     }
@@ -27,7 +25,13 @@ mod tests {
     }
 
     #[test]
-    fn little_complicated() {
+    fn little_temperatures() {
+        let temperatures = vec![30, 40, 50, 60];
+        assert_eq!(daily_temperatures(temperatures), vec![1, 1, 1, 0]);
+    }
+
+    #[test]
+    fn complicated() {
         let temperatures = vec![73, 74, 75, 71, 69, 72, 76, 73];
         assert_eq!(
             daily_temperatures(temperatures),
