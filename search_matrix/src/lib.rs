97d81dc6251b8276e3a57cbd9b458a395c585c5e
siddharthteli12 <telisiddharth123@yahoo.in>
2023-11-24 09:48:59 +0530

    Add: Init search matrix

---
+++ search_matrix/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+        let flattes: Vec<&i32> = matrix.iter().flatten().collect();
+        flattes.binary_search(&&target).is_ok()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let matrix = vec![vec![1,3,5,7],vec![10,11,16,20],vec![23,30,34,60]];
+        let target = 3;
+        assert_eq!(search_matrix(matrix, target), true);
+    }
+}
